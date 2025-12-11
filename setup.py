467e6ca65f4e4f8aa468391c7b2f4ba1d67aa76f
Davide Bincoletto <davibinco@gmail.com>
2025-12-11 16:51:00 +0100

    update for PyPi

--- setup.py
+++ setup.py
@@ -8,12 +8,32 @@ def read_requirements():
         requirements = [line.strip() for line in lines]
     return requirements
 
+# get author and version information
+VERSIONFILE = "src/sunrise/version.py"
+info = {"__version__": None, "__author__": None}
+with open(VERSIONFILE, "r") as f:
+    for l in f.readlines():
+        tmp = l.split("=")
+        if tmp[0].strip().lower() in info:
+            info[tmp[0].strip().lower()] = tmp[1].strip().strip('"').strip("'")
+
+for k, v in info.items():
+    if v is None:
+        raise Exception("could not find {} string in {}".format(k, VERSIONFILE))
+
+try:
+    with open("README.md", "r") as f:
+        long_description = f.read()
+except Exception:
+    long_description = ""
 
 setup(
     name='project-sunrise',
     version="0.1.0",
     author="Chair of Quantum Algorithmics at Augsburg University (Germany)",
     url="https://github.com/tequilahub/sunrise",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=read_requirements(),
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
